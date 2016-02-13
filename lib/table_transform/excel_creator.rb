ee4d0fd4516b07ab1b0866c7b02c704037b185b8
Jonas Lantto <j@lantto.net>
2016-02-13 15:23:05 +0100

    Converted ExcelCreator to class

--- lib/table_transform/excel_creator.rb
+++ lib/table_transform/excel_creator.rb
@@ -1,55 +1,67 @@
 require 'write_xlsx'
 
+
 module TableTransform
-  module ExcelCreator
-    module Util
-      # @return array with column width per column
-      def self.column_width(data, auto_filter_correct = true, max_width = 100)
-        return [] if data.empty?
 
-        auto_filter_size_correction = auto_filter_correct ? 3 : 0
-
-        res = Array.new(data.first.map { |x| x.to_s.size + auto_filter_size_correction })
-        data.each { |row|
-          row.each_with_index { |cell, column_no|
-            res[column_no] = [cell.to_s.size, res[column_no]].max
-          }
-        }
-        res.map! { |x| [x, max_width].min }
-      end
-
-
-      def self.create_table(workbook, name, data)
-        worksheet = workbook.add_worksheet(name)
-        return if data.nil? or data.empty? # Create empty worksheet if no data
-
-        col_width = column_width(data)
-
-        header = data.shift
-        data << [nil] * header.count if data.empty? # Add extra row if empty
-
-        worksheet.add_table(
-            0, 0, data.count, header.count - 1,
-            {
-                :name => name.tr(' ', '_'),
-                :data => data,
-                :autofilter => 1,
-                :columns => header.map { |v| {:header => v} }
-            }
-        )
-
-        # Set column width
-        col_width.each_with_index { |size, column| worksheet.set_column(column, column, size)}
-      end
+  # Creates excel file
+  class ExcelCreator
+    def initialize(filename)
+      @workbook = WriteXLSX.new(filename)
+      ObjectSpace.define_finalizer(self, self.class.finalize(@workbook))
     end
 
-    #Creates excel sheet with given filename and given tab(s)
-    def self.create_workbook(filename, tabs = {})
-      workbook = WriteXLSX.new(filename)
-      tabs.each { |name, data|
-        Util::create_table(workbook, name, data)
+    def self.finalize(workbook)
+      proc { workbook.close }
+    end
+
+    def close
+      @workbook.close
+    end
+
+    def add_tab(name, data)
+      create_table(name, data)
+    end
+
+
+    private
+
+    # @return array with column width per column
+    def self.column_width(data, auto_filter_correct = true, max_width = 100)
+      return [] if data.empty?
+
+      auto_filter_size_correction = auto_filter_correct ? 3 : 0
+
+      res = Array.new(data.first.map { |x| x.to_s.size + auto_filter_size_correction })
+      data.each { |row|
+        row.each_with_index { |cell, column_no|
+          res[column_no] = [cell.to_s.size, res[column_no]].max
+        }
       }
-      workbook.close
+      res.map! { |x| [x, max_width].min }
+    end
+
+    def create_table(name, data)
+      worksheet = @workbook.add_worksheet(name)
+      return if data.nil? or data.empty? # Create empty worksheet if no data
+
+      auto_filter = true
+      col_width = ExcelCreator::column_width(data, auto_filter)
+
+      header = data.shift
+      data << [nil] * header.count if data.empty? # Add extra row if empty
+
+      worksheet.add_table(
+          0, 0, data.count, header.count - 1,
+          {
+              :name => name.tr(' ', '_'),
+              :data => data,
+              :autofilter => auto_filter ? 1 : 0,
+              :columns => header.map { |v| {:header => v} }
+          }
+      )
+
+      # Set column width
+      col_width.each_with_index { |size, column| worksheet.set_column(column, column, size) }
     end
   end
 end
--- test/excel_creator_test.rb
+++ test/excel_creator_test.rb
@@ -5,7 +5,9 @@ require 'table_transform/excel_creator'
 require 'roo'
 require 'securerandom'
 
+
 class ExcelCreatorTest < Minitest::Test
+
   def setup
     @tmp_filename = Pathname(Dir.tmpdir) + ('TableTransform' + SecureRandom.uuid + '.xlsx')
   end
@@ -15,32 +17,35 @@ class ExcelCreatorTest < Minitest::Test
 
   def test_column_width
     # empty
-    assert_equal([], TableTransform::ExcelCreator::Util.column_width([], true))
+    assert_equal([], TableTransform::ExcelCreator::column_width([], true))
 
     #header and row
     data = [['AAA', 23],
             ['BBBB', 4]]
-    assert_equal([6,5], TableTransform::ExcelCreator::Util.column_width(data, true), 'Auto filter correction')
-    assert_equal([4,2], TableTransform::ExcelCreator::Util.column_width(data, false))
+    assert_equal([6,5], TableTransform::ExcelCreator::column_width(data, true), 'Auto filter correction')
+    assert_equal([4,2], TableTransform::ExcelCreator::column_width(data, false))
 
     #maximum header width per column
     data = [['333', '4444', 55555]]
-    assert_equal([3,4,4], TableTransform::ExcelCreator::Util.column_width(data, false, 4))
+    assert_equal([3,4,4], TableTransform::ExcelCreator::column_width(data, false, 4))
 
     #different types
     data = [[12.0, 33.33, nil]]
-    assert_equal([4,5,0], TableTransform::ExcelCreator::Util.column_width(data, false))
+    assert_equal([4,5,0], TableTransform::ExcelCreator::column_width(data, false))
   end
 
   def test_excel_create
     data1 = [%w(Col1 Col2),
             ['StR', 32],
             [nil, 65.23]]
-    TableTransform::ExcelCreator::create_workbook(@tmp_filename, {'data1' => data1,
-                                                                  'Data Header only' => [%w(Col1 Col2)],
-                                                                  'Data Nil' => nil,
-                                                                  'Data Empty' => []})
 
+    excel = TableTransform::ExcelCreator.new(@tmp_filename)
+    excel.add_tab('data1', data1)
+    excel.add_tab('Data Header only', [%w(Col1 Col2)])
+    excel.add_tab('Data Nil', nil)
+    excel.add_tab('Data Empty', [])
+    excel.close                                                              
+    
     xlsx = Roo::Excelx.new(@tmp_filename)
     assert_equal(['data1', 'Data Header only','Data Nil', 'Data Empty'], xlsx.sheets)
 
@@ -77,5 +82,4 @@ class ExcelCreatorTest < Minitest::Test
       assert_equal(nil,     sheet.cell(1, 'A'))
 
   end
-
 end
